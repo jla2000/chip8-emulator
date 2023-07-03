bc8aeb60020c3f695b5eb5e2dd2a54b00e2f59d8
Jan Lafferton <jan@lafferton.de>
2023-07-03 16:03:11 +0200

    Update readme

--- README.md
+++ README.md
@@ -11,6 +11,17 @@ My own take on a very simple chip8 emulator, using rust and the wgpu project.
 - Working Keyboard & Audio
 
 
+## Usage
+
+Download any chip8 rom from the internet.
+Eg. _space_invaders.ch8_:
+
+```sh
+cd chip8-emulator
+cargo run -- space_invaders.ch8
+```
+
+
 ## Screenshots
 
 ![Screenshot 1](./screenshots/1.png)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 use dotenv::dotenv;
 use rodio::OutputStream;
 use rodio::Sink;
@@ -13,13 +15,13 @@ use render::*;
 use sound::*;
 use window::*;
 
-async fn run() {
+async fn run(rom: &[u8]) {
     let mut window_state = create_window();
     let mut chip8_state = Chip8State::new();
     let mut renderer = Renderer::new(&window_state.window).await;
     let mut beeper = Beeper::new();
 
-    chip8_state.load_rom(include_bytes!(r"../space_invaders.ch8"));
+    chip8_state.load_rom(rom);
 
     while !window_state.window.should_close() {
         window_state.glfw.poll_events();
@@ -46,9 +48,24 @@ async fn run() {
     }
 }
 
+fn read_rom(filename: &str) -> std::io::Result<Vec<u8>> {
+    let f = std::fs::File::open(filename)?;
+    let mut reader = std::io::BufReader::new(f);
+    let mut buffer = Vec::new();
+
+    reader.read_to_end(&mut buffer)?;
+    Ok(buffer)
+}
+
 fn main() {
     dotenv().ok();
     env_logger::init();
 
-    pollster::block_on(run());
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        println!("Usage: chip8-emulator <ROM_FILE>");
+    } else {
+        let rom = read_rom(&args[1]).unwrap();
+        pollster::block_on(run(rom.as_slice()));
+    }
 }
